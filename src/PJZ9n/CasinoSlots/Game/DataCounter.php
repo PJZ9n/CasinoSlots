1bd286f5f4a295ebe6be27f5b9d2be4b6c14fd43
PJZ9n <pjz9n@yahoo.co.jp>
2020-02-09 04:52:45 +0900

    データカウンターのインターフェースを作成

---
+++ src/PJZ9n/CasinoSlots/Game/DataCounter.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright (c) 2020 PJZ9n.
+ * This file is part of CasinoSlots.
+ * CasinoSlots is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * CasinoSlots is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CasinoSlots.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace PJZ9n\CasinoSlots\Game;
+
+interface DataCounter
+{
+    
+    /**
+     * データカウンターを文字列で取得する
+     *
+     * @return string
+     */
+    public function getStringDataCounter(): string;
+    
+}
